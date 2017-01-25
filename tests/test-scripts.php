1890fdc2e0a7a8c8ab88ddb161e287708712eea2
Milan Dinić <forumi@srpski.biz>
2017-01-25 19:49:40 +0100

    Add scripts tests.

---
+++ tests/test-scripts.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Class Tests_Use_unpkg
+ *
+ * @package Use_unpkg
+ * @subpackage Test
+ */
+
+/**
+ * Test case for transients.
+ *
+ * @since 1.0
+ */
+class Tests_Use_unpkg_Scripts extends WP_UnitTestCase {
+	/**
+	 * Test that scripts sources are replaced.
+	 *
+	 * @since 1.0
+	 * @access public
+	 */
+	public function test_scripts_replaced() {
+		$scripts = wp_scripts();
+		foreach ( Use_unpkg::get_instance()->unpkg_scripts as $handle => $data ) {
+			$script = $scripts->query( $handle );
+			$this->assertContains(
+				'https://unpkg.com',
+				$script->src, $handle . ' should be loading from unpkg'
+			);
+		}
+	}
+
+	/**
+	 * Test that remote files exist.
+	 *
+	 * @since 1.0
+	 * @access public
+	 */
+	public function test_scripts_exists_on_unpkg() {
+		$scripts = wp_scripts();
+		foreach ( Use_unpkg::get_instance()->unpkg_scripts as $handle => $data ) {
+			$script = $scripts->query( $handle );
+			$response_code = wp_remote_retrieve_response_code( wp_remote_get( $script->src ) );
+			$this->assertEquals(
+				200,
+				$response_code,
+				$handle . ' should exists on unpkg'
+			);
+		}
+	}
+
+	public function test_noconflict_injected() {
+		$script = $scripts->query( 'jquery-core' );
+		$data = $script->extra['after'][1];
+		$this->assertEquals(
+			'try{jQuery.noConflict();}catch(e){};',
+			$data,
+			'jQuery should be in noconlict mode'
+		);
+	}
+}
