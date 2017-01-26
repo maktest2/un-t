0d11f68bd0b5702a27d996c859ec19589bc828a2
Milan Dinić <forumi@srpski.biz>
2017-01-26 20:28:24 +0100

    Test now.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,16 +21,5 @@ function _manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-function _copy_original_dependency_src( $scripts ) {
-	foreach ( Use_unpkg::get_instance()->unpkg_scripts as $handle => $data ) {
-		if ( in_array( $handle, array( 'twentysixteen-html5', 'html5', 'jquery-scrollto' ) ) ) {
-			continue;
-		}
-		$script = $scripts->query( $handle );
-		$script->original_src = $script->src;
-	}
-}
-tests_add_filter( 'wp_default_scripts', '_copy_original_dependency_src', 11 );
-
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
--- tests/test-scripts.php
+++ tests/test-scripts.php
@@ -14,21 +14,10 @@
  * @since 1.0
  */
 class Tests_Use_unpkg_Scripts extends WP_UnitTestCase {
-	function setUp() {
-		parent::setUp();
-echo 'setup me' . "\n";
-echo current_filter() . "\n";
-		add_action( 'wp_default_scripts', array( $this, 'copy_original_src' ) );
-	}
-
 	public function copy_original_src( $scripts ) {
-		echo 'copy me' . "\n";
-		print_r( $scripts );
-		echo "\n";
 		foreach ( Use_unpkg::get_instance()->unpkg_scripts as $handle => $data ) {
 			$script = $scripts->query( $handle );
-			echo $handle . "\n";
-			print_r( $script );
+
 			$script->original_src = $script->src;
 		}
 	}
@@ -44,10 +33,8 @@ echo current_filter() . "\n";
 		$s = Use_unpkg::get_instance()->unpkg_scripts;
 	
 		foreach ( $s as $handle => $data ) {
-			echo $handle . "\n";
 			if ( in_array( $handle, array( 'twentysixteen-html5', 'html5', 'jquery-scrollto' ) ) ) {
 				continue;
-				//$this->markTestSkipped( 'Twenty Sixteen should be active for this test' );
 			}
 			
 			$script = $scripts->query( $handle );
@@ -55,9 +42,7 @@ echo current_filter() . "\n";
 				'https://unpkg.com',
 				$script->src, $handle . ' should be loading from unpkg'
 			);
-			echo $script->src . "\n";
 		}
-echo current_filter() . "\n";
 	}
 
 	/**
@@ -71,52 +56,14 @@ echo current_filter() . "\n";
 		foreach ( Use_unpkg::get_instance()->unpkg_scripts as $handle => $data ) {
 			if ( in_array( $handle, array( 'twentysixteen-html5', 'html5', 'jquery-scrollto' ) ) ) {
 				continue;
-				//$this->markTestSkipped( 'Twenty Sixteen should be active for this test' );
 			}
-echo $handle . "\n";
 			$script = $scripts->query( $handle );
 			$response = wp_remote_get( $script->src );
 			$response_code = wp_remote_retrieve_response_code( $response );
-			//print_r( $response );
 			$this->assertEquals(
 				200,
 				$response_code,
 				$handle . ' should exists on unpkg'
-			);
-			echo $response_code . "\n";
-		}
-print_r( did_action( 'init' ) . "\n" );
-print_r( did_action( 'wp' ) . "\n" );
-print_r( did_action( 'template_redirect' ) . "\n" );
-print_r( did_action( 'wp_head' ) . "\n" );
-print_r( did_action( 'wp_footer' ) . "\n" );
-	}
-
-	/**
-	 * Test that original files are same.
-	 *
-	 * @since 1.0
-	 * @access public
-	 */
-	public function test_scripts_originals_and_unpkg_same() {
-		$scripts = wp_scripts();
-
-		foreach ( Use_unpkg::get_instance()->unpkg_scripts as $handle => $data ) {
-			if ( in_array( $handle, array( 'twentysixteen-html5', 'html5', 'jquery-scrollto' ) ) ) {
-				continue;
-				//$this->markTestSkipped( 'Twenty Sixteen should be active for this test' );
-			}
-
-			$script = $scripts->query( $handle );
-			$original_content = file_get_contents( untrailingslashit( ABSPATH ) . $script->original_src );
-			$response_body = wp_remote_retrieve_body( wp_remote_get( $script->src ) );
-			if ( 'jquery-core' == $handle ) {
-				continue;//$response_body .= 'jQuery.noConflict();';
-			}
-			$this->assertEquals(
-				$original_content,
-				$response_body,
-				$handle . ' should be the same on unpkg'
 			);
 		}
 	}
