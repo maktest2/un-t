e68943368ce096ed4cb7e3d7fcc9b3b3b02d1452
Milan Dinić <forumi@srpski.biz>
2017-01-26 19:48:12 +0100

    what now

--- tests/test-scripts.php
+++ tests/test-scripts.php
@@ -111,8 +111,7 @@ print_r( did_action( 'wp_footer' ) . "\n" );
 			$original_content = file_get_contents( untrailingslashit( ABSPATH ) . $script->original_src );
 			$response_body = wp_remote_retrieve_body( wp_remote_get( $script->src ) );
 			if ( 'jquery-core' == $handle ) {
-				$response_body .= '
-jQuery.noConflict();';
+				$response_body .= 'jQuery.noConflict();';
 			}
 			$this->assertEquals(
 				$response_body,
